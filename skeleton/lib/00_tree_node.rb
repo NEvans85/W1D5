6acf87e496603bdd8913080212fedb11fc4cc07e
App Academy Student <student@appacademy.io>
2017-08-04 12:19:06 -0700

    pre lunch commit

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,51 @@
+require 'byebug'
+
+
 class PolyTreeNode
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+  def parent=(node)
+   # TODO: refactor conditional
+    if @parent != node
+      @parent.children.delete(self) unless @parent.nil?
+    end
+    @parent = node
+    @parent.children << self unless @parent.nil? || @parent.children.include?(self)
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def add_child(child_node)
+    child_node.parent=(self)
+  end
+
+  def remove_child(child_node)
+    unless @children.include?(child_node)
+      raise 'Not one of mine!'
+    end
+    child_node.parent=(nil)
+  end
+
+  def dfs(target_value)
+    return self if @value == target_value
+    @children.each do |child|
+      puts child.dfs(target_value)
+    end
+    nil
+  end
 
 end
