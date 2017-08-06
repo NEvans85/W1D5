53b6d15f19f5b1043607e0f54bf1bd463cc12da5
Nicholas Evans <nevans85.pro@gmail.com>
2017-08-05 21:51:26 -0700

    finish Knight_Travails.rb

--- Knight_Travails.rb
+++ Knight_Travails.rb
@@ -6,21 +6,39 @@ class KnightPathFinder
   attr_reader :position, :move_tree, :board, :visited_positions
 
   def initialize(init_pos)
-    @position = init_pos
+    @init_pos = init_pos
+    @visited_positions = [init_pos]
     @move_tree = build_move_tree
     @board = Array.new(8) { Array.new(8) }
-    @visited_positions = []
   end
 
   def build_move_tree
-    queue = [@position]
+    root = PolyTreeNode.new(@init_pos)
+    queue = [root]
+    tree = [root]
     until queue.empty?
+      parent = queue.shift
+      new_move_positions(parent.value).each do |pos|
+        child = PolyTreeNode.new(pos)
+        child.parent = parent
+        queue << child
+        tree << child
+      end
+    end
+    tree
   end
 
-  def find_path
+  def find_path(pos)
+    trace_path_back(@move_tree.first.bfs(pos))
+  end
+
+  def trace_path_back(node)
+    return [node.value] if node.parent.nil?
+    trace_path_back(node.parent) + [node.value]
   end
 
   def new_move_positions(pos)
+    # debugger
     unvisited_positions = valid_moves(pos).reject do |valid_pos|
       @visited_positions.include?(valid_pos)
     end
