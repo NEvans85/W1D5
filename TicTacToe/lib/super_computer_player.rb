4ea4e34388c6572ec0691317ac8a92232f720cbe
Nicholas Evans <nevans85.pro@gmail.com>
2017-08-06 08:54:40 -0700

    write super_computer_player (non-functional)

--- TicTacToe/lib/super_computer_player.rb
+++ TicTacToe/lib/super_computer_player.rb
@@ -1,7 +1,17 @@
 require_relative 'tic_tac_toe_node'
+require 'byebug'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+     debugger
+    root = TicTacToeNode.new(game.board, mark)
+    root.children.each do |node|
+      return node.prev_move_pos if node.winning_node?(mark)
+    end
+    root.children.each do |node|
+      return node.prev_move_pos unless node.losing_node?(mark)
+    end
+    raise 'Come On AI, do better!'
   end
 end
 
